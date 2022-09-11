aac1ca0eecd32ccba29a3132edba71fcac937204
skrowll <lucasdejesus.rocha@hotmail.com>
2022-09-10 21:12:39 -0300

    refactor: refac port on index.js

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Escutando na porta 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Escutando na porta ${port}`);
 });
